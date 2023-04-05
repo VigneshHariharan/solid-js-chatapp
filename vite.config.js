a4cd90e27ce2e04c08a034e8a2b3836b4b378f90
VigneshHariharan <vignesh@Vigneshs-MacBook-Air.local>
2023-04-05 07:04:48 +0530

    fix: prod build treeshake smallest

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,23 @@
-import { defineConfig } from 'vite';
-import solidPlugin from 'vite-plugin-solid';
+import { defineConfig } from "vite";
+import solidPlugin from "vite-plugin-solid";
+import { splitVendorChunkPlugin } from "vite";
 
 export default defineConfig({
-  plugins: [solidPlugin()],
+  plugins: [solidPlugin(), splitVendorChunkPlugin()],
+
   server: {
     port: 3000,
   },
+
+  resolve: {
+    alias: {
+      "./runtimeConfig": "./runtimeConfig.browser",
+    },
+  },
   build: {
-    target: 'esnext',
+    target: "esnext",
+    rollupOptions: {
+      treeshake: "smallest",
+    },
   },
 });
